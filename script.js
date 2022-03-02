8cdc54b4869d03641fa3a2584c8e1f3471df6feb
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-02 21:40:43 +0600

    align photo

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const showPhones = phones => {
       const div = document.createElement("div");
       div.classList.add("card");
       div.innerHTML = `
-      <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
+      <img src="${phone.image}" class="card-img-top w-50 m100" alt="...">
       <div class="card-body">
       <h5 class="card-title">${phone.phone_name}</h5>
       <p class="card-text">Brand: ${phone.brand}</p>
